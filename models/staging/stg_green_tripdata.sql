c86961d9906b24a13970dee0a967081cc8ee6cd0
MARCELLINUS WITARSAH <aw.marcel@gmail.com>
2025-04-12 10:13:11 +0000

    Add dim_zones as a reference table for pickup and drop off zone. Add fact_trips as a final data model for consumption for dashboards or end users.

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -19,34 +19,33 @@ select
 
     -- identifiers
     {{ dbt_utils.generate_surrogate_key(['filename', 'vendorid', 'lpep_pickup_datetime', 'lpep_dropoff_datetime']) }} as trip_id,
-    filename,
-    vendorid,
-
+    cast(filename as string) as filename,
+    {{ dbt.safe_cast("vendorid", api.Column.translate_type("integer")) }}as vendorid,
+    {{ dbt.safe_cast("ratecodeid", api.Column.translate_type("integer")) }}as ratecodeid,
+    {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }}as pickup_location_id,
+    {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }}as dropoff_location_id,
     -- timestamps
     cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
     cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
 
     -- trip information
     store_and_fwd_flag,
-    ratecodeid,
-    pulocationid,
-    dolocationid,
-    passenger_count,
-    trip_distance,
+    {{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} as passenger_count,
+    cast(trip_distance as numeric) as trip_distance,
+    {{ dbt.safe_cast("trip_type", api.Column.translate_type("integer")) }} as trip_type,
 
-    -- price information
-    fare_amount,
-    extra,
-    mta_tax,
-    tip_amount,
-    tolls_amount,
-    ehail_fee,
-    improvement_surcharge,
-    total_amount,
-    payment_type,
-    {{ get_payment_type_description('payment_type') }} as payment_type_description, 
-    trip_type,
-    congestion_surcharge
+
+    -- payment info
+    cast(fare_amount as numeric) as fare_amount,
+    cast(extra as numeric) as extra,
+    cast(mta_tax as numeric) as mta_tax,
+    cast(tip_amount as numeric) as tip_amount,
+    cast(tolls_amount as numeric) as tolls_amount,
+    cast(ehail_fee as numeric) as ehail_fee,
+    cast(improvement_surcharge as numeric) as improvement_surcharge,
+    cast(total_amount as numeric) as total_amount,
+    coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type,
+    {{ get_payment_type_description("payment_type") }} as payment_type_description
 
 from tripdata
 where rn=1
--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -16,31 +16,34 @@ with tripdata as (
 select
     -- identifiers
     {{ dbt_utils.generate_surrogate_key(['filename', 'vendorid', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']) }} as trip_id,
-    filename,
-    vendorid,
+    cast(filename as string) as filename,
+    {{ dbt.safe_cast("vendorid", api.Column.translate_type("integer")) }}as vendorid,
+    {{ dbt.safe_cast("ratecodeid", api.Column.translate_type("integer")) }}as ratecodeid,
+    {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }}as pickup_location_id,
+    {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }}as dropoff_location_id,
+
     -- timestamps
     cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
     cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
 
     -- triap information
-    passenger_count,
-    trip_distance,
-    ratecodeid,
     store_and_fwd_flag,
-    pulocationid,
-    dolocationid,
+    {{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} as passenger_count,
+    cast(trip_distance as numeric) as trip_distance,
+    -- yellow cabs are always street-hail
+    1 as trip_type,
 
     -- payment information
-    payment_type,
-    {{ get_payment_type_description('payment_type') }} as payment_type_description,
-    fare_amount,
-    extra,
-    mta_tax,
-    tip_amount,
-    tolls_amount,
-    improvement_surcharge,
-    total_amount,
-    congestion_surcharge
+    cast(fare_amount as numeric) as fare_amount,
+    cast(extra as numeric) as extra,
+    cast(mta_tax as numeric) as mta_tax,
+    cast(tip_amount as numeric) as tip_amount,
+    cast(tolls_amount as numeric) as tolls_amount,
+    cast(0 as numeric) as ehail_fee,
+    cast(improvement_surcharge as numeric) as improvement_surcharge,
+    cast(total_amount as numeric) as total_amount,
+    coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type,
+    {{ get_payment_type_description('payment_type') }} as payment_type_description
 
 from tripdata
 where rn=1
